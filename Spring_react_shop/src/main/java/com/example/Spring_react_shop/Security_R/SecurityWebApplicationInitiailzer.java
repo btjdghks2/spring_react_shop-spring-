e8a37dd1468427b30412e18bdac75a0eafd27478
btjdghks2 <btjdghks2@gmail.com>
2023-02-03 11:05:48 +0900

    security Example1

---
+++ Spring_react_shop/src/main/java/com/example/Spring_react_shop/Security_R/SecurityWebApplicationInitiailzer.java
@@ -0,0 +1,6 @@
+package com.example.Spring_react_shop.Security_R;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitiailzer extends AbstractSecurityWebApplicationInitializer {
+}
